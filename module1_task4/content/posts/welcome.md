8012ff7a127822584f13a3ec8de6515c3567e665
john <john@localhost.localdomain>
2021-07-28 22:49:46 +0400

    final commit

---
+++ module1_task4/content/posts/welcome.md
@@ -0,0 +1,6 @@
+---
+title: "Welcome"
+date: 2021-07-28T18:38:30Z
+draft: false
+---
+
--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,9 +1,12 @@
-apt-get update && apt-get install -y make hugo
+apt-get update && apt-get install -y make wget unzip
+wget -q https://github.com/gohugoio/hugo/releases/download/v0.86.0/hugo_0.86.0_Linux-64bit.deb
+dpkg -i hugo_0.86.0_Linux-64bit.deb
 hugo new site ./ --force
-#wget -q https://github.com/theNewDynamic/gohugo-theme-ananke/archive/master.zip
-#unzip master.zip
-#mv gohugo-theme-ananke-master ananke
-#mv ananke/ themes/
-#echo title = \"Awesome Inc.\" > config.toml
+wget -q https://github.com/theNewDynamic/gohugo-theme-ananke/archive/master.zip
+unzip master.zip
+mv gohugo-theme-ananke-master ananke
+mv ananke/ themes/
+sed -i -e 's/title = \".*\"/title = \"Awesome Inc.\"'
 #echo theme = \"ananke\" >> config.toml
 echo publishDir = \"dist/\" >> config.toml
+make build
